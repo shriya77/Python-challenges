e846061888840cbf3bad81c00b05248b96808eac
Shriya Kalyan <shriyakkx@gmail.com>
2022-12-10 16:52:00 +0800

    Update TT01 say hello.py

--- Turtle (TU)/TT01 say hello.py
+++ Turtle (TU)/TT01 say hello.py
@@ -1,6 +1,5 @@
 import turtle
-j=input("What would you like me to write? ")
-turtle.write(j)
-turtle.forward(7*len(j))
+turtle.write("Hello")
+turtle.forward(100)
 turtle.shape("turtle")
 turtle.done()
